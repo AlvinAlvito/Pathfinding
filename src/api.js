c1eeccd7d2f247e8b6db0a8d6a4c0c69c1d0fb4a
AlvinAlvito <alvinparis65@gmail.com>
2024-11-03 09:25:29 +0700

    add

--- src/api.js
+++ src/api.js
@@ -21,3 +21,10 @@ export function fetchOverpassData(boundingBox) {
 }
 
 // https://overpass-api.de/api/interpreter?data=[out:json];(way[highway][highway!="footway"][highway!="street_lamp"][highway!="steps"][highway!="pedestrian"][highway!="track"][highway!="path"][footway!="*"](-7.7956,110.3695,-7.7925,110.3777);node(w););out skel;
+// [out:json];
+// (
+//   node(around:10, 3.5482131, 98.6627623);
+//   node(around:10, 3.5485234, 98.6614430);
+//   node(around:10, 3.5487161, 98.6613142);
+// );
+// out body;
